d4d2baa716bef6a3966e26273b263a19ccb08a05
Nicholas <neoxb@yahoo.com>
2020-08-30 21:59:26 +1200

    30/08/2020

--- domainmodel/test_watchlist.py
+++ domainmodel/test_watchlist.py
@@ -1,11 +1,99 @@
+from domainmodel.genre import Genre
 from domainmodel.movie import Movie
+from domainmodel.user import User
 from domainmodel.watchlist import WatchList
+import pytest
 
+@pytest.fixture()
+def w():
+    return WatchList(User('ABC','123'),"")
 
-def test_iter():
-    w = WatchList()
+def test_default_name(w):
+    assert w.watchlist_name == "New Watchlist"
+
+def test_iter_and_next(w):
     w.add_movie(Movie("Moana", 2016))
     w.add_movie(Movie("Ice Age", 2002))
     w.add_movie(Movie("Guardians of the Galaxy", 2012))
     iterable = iter(w)
-    assert(next(iterable) == Movie("Moana", 2016)) == True
+    assert next(iterable) == Movie("Moana", 2016)
+
+def test_add_and_remove_movie(w):
+    w.add_movie(Movie("Moana", 2016))
+    w.remove_movie(Movie("Moana", 2016))
+    assert w.watchlist == []
+
+def test_select_movie(w):
+    w.add_movie(Movie("Moana", 2016))
+    assert w.select_movie_to_watch(0) == Movie("Moana", 2016)
+
+def test_size(w):
+    w.add_movie(Movie("Moana", 2016))
+    assert w.size() == 1
+
+def test_first_movie_in_watchlist(w):
+    w.add_movie(Movie("Moana", 2016))
+    w.add_movie(Movie("Ice Age", 2002))
+    w.add_movie(Movie("Guardians of the Galaxy", 2012))
+    assert w.first_movie_in_watchlist() == Movie("Moana", 2016)
+
+def test_clear_watchlist(w):
+    w.add_movie(Movie("Moana", 2016))
+    w.add_movie(Movie("Ice Age", 2002))
+    w.add_movie(Movie("Guardians of the Galaxy", 2012))
+    w.clear_watchlist()
+    assert w.watchlist == []
+
+def test_share_watchlist(w):
+    w.add_movie(Movie("Moana", 2016))
+    w.add_movie(Movie("Ice Age", 2002))
+    w.add_movie(Movie("Guardians of the Galaxy", 2012))
+    user1 = User("DEF","wow")
+    w1 = w.share_watchlist(user1)
+    assert w1.watchlist_owner == user1
+    assert w1.watchlist == w.watchlist
+
+def test_sort_by_title(w):
+    m1 = Movie("Moana", 2016)
+    m2 = Movie("Ice Age", 2002)
+    m3 = Movie("Guardians of the Galaxy", 2012)
+    w.add_movie(m1)
+    w.add_movie(m2)
+    w.add_movie(m3)
+    watchlist = w.sort_watchlist_by_title()
+    assert watchlist == [m3,m2,m1]
+
+def test_sort_by_year(w):
+    m1 = Movie("Moana", 2016)
+    m2 = Movie("Ice Age", 2002)
+    m3 = Movie("Guardians of the Galaxy", 2012)
+    w.add_movie(m1)
+    w.add_movie(m2)
+    w.add_movie(m3)
+    watchlist = w.sort_watchlist_by_year()
+    assert watchlist == [m2,m3,m1]
+
+def test_sort_by_runtime(w):
+    m1 = Movie("Moana", 2016)
+    m2 = Movie("Ice Age", 2002)
+    m3 = Movie("Guardians of the Galaxy", 2012)
+    m1.runtime_minutes = 1
+    m2.runtime_minutes = 100
+    m3.runtime_minutes = 10
+    w.add_movie(m1)
+    w.add_movie(m2)
+    w.add_movie(m3)
+    watchlist = w.sort_watchlist_by_runtime()
+    assert watchlist == [m1,m3,m2]
+
+def test_change_watchlist_name(w):
+    w.change_watchlist_name("WOW")
+    assert w.watchlist_name == "WOW"
+
+def test_get_recommendations(w):
+    m1 = Movie("Action",2000)
+    m1.genres = [Genre("Crime"),Genre("Drama"),Genre("Horror")]
+    w.add_movie(m1)
+    w1 = w.get_recommendations("Data1000Movies.csv")
+    movies = [Movie("Hounds of Love", 2016), Movie("Funny Games", 2007), Movie("The Girl Next Door", 2007)]
+    assert w1.watchlist == movies
--- domainmodel/watchlist.py
+++ domainmodel/watchlist.py
@@ -1,13 +1,32 @@
+from datafilereaders.movie_file_csv_reader import MovieFileCSVReader
 from domainmodel.movie import Movie
+from domainmodel.user import User
+
 
 class WatchList:
 
-    def __init__(self):
+    def __init__(self, user: User, watchlist_name: str):
+        if type(user) is not User:
+            raise Exception("Sorry, that is an invalid User")
+        else:
+            self.__watchlist_owner = user
+        if type(watchlist_name) is not str or watchlist_name == "":
+            self.__watchlist_name = "New Watchlist"
+        else:
+            self.__watchlist_name = watchlist_name.strip()
         self.__watchlist = list()
 
     @property
     def watchlist(self):
         return self.__watchlist
+
+    @property
+    def watchlist_owner(self):
+        return self.__watchlist_owner
+
+    @property
+    def watchlist_name(self):
+        return self.__watchlist_name
 
     def __iter__(self):
         self.__index = 0
@@ -49,3 +68,52 @@ class WatchList:
             return None
         else:
             return self.__watchlist[0]
+
+    def clear_watchlist(self):
+        self.__watchlist.clear()
+
+    def share_watchlist(self, user):
+        if not isinstance(user, User):
+            raise Exception("A valid User is required")
+        else:
+            new_watchlist = WatchList(user,"")
+            for movie in self.__watchlist:
+                new_watchlist.add_movie(movie)
+            return new_watchlist
+
+    def sort_watchlist_by_title(self):
+        def get_movie_title(movie):
+            return movie.title
+        return sorted(self.__watchlist, key = get_movie_title)
+
+    def sort_watchlist_by_year(self):
+        def get_movie_year(movie):
+            return movie.release_year
+        return sorted(self.__watchlist, key = get_movie_year)
+
+    def sort_watchlist_by_runtime(self):
+        def get_movie_runtime(movie):
+            return movie.runtime_minutes
+        return sorted(self.__watchlist, key = get_movie_runtime)
+
+    def change_watchlist_name(self, new_name):
+        if isinstance(new_name, str) or new_name != "":
+            self.__watchlist_name = new_name
+
+    def get_recommendations(self, filename):
+        if not isinstance(filename, str):
+            raise Exception("Invalid filename")
+        elif len(self.__watchlist) == 0:
+            raise Exception("Sorry, there are no recommendations for now")
+        else:
+            new_watchlist = WatchList(self.watchlist_owner,"Movie Recommendations")
+            movie_file_reader = MovieFileCSVReader(filename)
+            movie_file_reader.read_csv_file()
+            for new_movie in movie_file_reader.dataset_of_movies:
+                for current_movie in self.__watchlist:
+                    if sorted(new_movie.genres) == sorted(current_movie.genres):
+                        new_watchlist.add_movie(new_movie)
+            return new_watchlist
+
+
+
